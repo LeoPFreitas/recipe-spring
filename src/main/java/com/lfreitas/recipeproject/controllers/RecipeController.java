392bc461af3a75d5b17e769c0b601c3922078e2f
leonardo-w10 <leonardo.cadu2@gmail.com>
2020-09-27 09:44:48 -0300

    Add delete recipes and list ingredients

--- src/main/java/com/lfreitas/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/lfreitas/recipeproject/controllers/RecipeController.java
@@ -2,10 +2,12 @@ package com.lfreitas.recipeproject.controllers;
 
 import com.lfreitas.recipeproject.commands.RecipeCommand;
 import com.lfreitas.recipeproject.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
 
@@ -42,6 +44,16 @@ public class RecipeController {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id){
+
+        log.debug("Deleting id: " + id);
+
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
     }
 
 
--- src/main/java/com/lfreitas/recipeproject/services/RecipeService.java
+++ src/main/java/com/lfreitas/recipeproject/services/RecipeService.java
@@ -14,4 +14,7 @@ public interface RecipeService {
     RecipeCommand saveRecipeCommand(RecipeCommand command);
 
     RecipeCommand findCommandById(Long l);
+
+    void deleteById(Long idToDelete);
+
 }
